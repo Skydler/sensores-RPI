cba72e997fe494c6e1905d4d0142164ca4e8c175
ramegp <ramegp@gmail.com>
2019-04-27 15:19:50 -0300

    Modifique unos errores que tiraba

--- temperatura.py
+++ temperatura.py
@@ -12,10 +12,10 @@ Resistencia 10K entre VCC y DATA
 
 class Temperatura:
 
-    def __init__(self, pin=17, sensor=Adafruit_DHT.DHT11)
-    # Usamos el DHT11 que es compatible con el DHT12 
-    self._sensor = sensor 
-    self._data_pin = pin
+    def __init__(self, pin=17, sensor=Adafruit_DHT.DHT11):
+        # Usamos el DHT11 que es compatible con el DHT12 
+        self._sensor = sensor 
+        self._data_pin = pin
 
     def datos_sensor(self):
       """ Devuelve un diccionario con la temperatura y humedad """

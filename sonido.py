3d7b0acb97472f421b2f44f01f6300e045d3bfa8
ramegp <ramegp@gmail.com>
2019-04-27 16:27:23 -0300

    Modifique los problemas que me tiraba pero sigue sin funcionar

--- sonido.py
+++ sonido.py
@@ -11,7 +11,7 @@ D0  --> pin 11 (BCM17)
 
 class Sonido:
     
-    def __init_(self, canal=17)
+    def __init__(self, canal=22):
         self._canal = canal
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self._canal, GPIO.IN)
@@ -19,7 +19,7 @@ class Sonido:
         GPIO.setwarnings(False)
 
     def agregar_evento(self, funcion):
-        GPIO.add_event_detect(canal, GPIO.BOTH, callback=funcion, bouncetime=500) 
+        GPIO.add_event_detect(self._canal, GPIO.BOTH, callback=funcion, bouncetime=500) 
 
 if __name__ == "__main__":
     import time

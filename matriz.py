1ef0304025ecd7573aa05a4d4729f700da6ee109
Leonel Mandarino <leonelmandarino@gmail.com>
2019-04-30 23:56:11 -0300

    Cambiado el delay de la matriz a 0.8

--- matriz.py
+++ matriz.py
@@ -8,10 +8,10 @@ from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
 class Matriz:
-    def __init__(self, numero_matrices=1, orientacion=0, rotacion=0):
+    def __init__(self, numero_matrices=1, orientacion=0, rotacion=0, ancho=8, alto=8):
         self.font = [CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT]
         self.serial = spi(port=0, device=0, gpio=noop())
-        self.device = max7219(self.serial, cascaded=numero_matrices, rotate=rotacion)
+        self.device = max7219(self.serial, width=ancho, height=alto, cascaded=numero_matrices, rotate=rotacion)
     
     def mostrar_mensaje(self, msg, delay=0.1, font=1):
         show_message(self.device, msg, fill="white",
